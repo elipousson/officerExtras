d7cda73e7ccb47c5e7ee98635198e159c3a9d724
Eli Pousson <eli.pousson@gmail.com>
2023-02-25 21:53:20 -0500

    feat: add add_vec_to_body function
    - feat: add add_vec_to_body function to work w/ vectorized value objects
    - refactor: make keyword, id, + index optional for add_to_body
    - refactor: make add_to_body use officer::body_add_par by default (since it respects the pos parameter)
    - refactor: add helper functions from standalone-purrr.R

--- R/add_to_body.R
+++ R/add_to_body.R
@@ -1,11 +1,21 @@
 
 #' Add an xml string or paragraph of text at a specified position in a Word document
 #'
-#' Wrappers for [officer::body_add()] and [officer::body_add_xml()] that use the
-#' [cursor_docx()] helper functions to allow input of filename and path and use
-#' of multiple options for cursor placement. [add_text_to_body()] passed value
-#' to [glue::glue()] to add support for glue string interpolation.
-#' [add_gt_to_body()] converts gt tables to OOXML with [gt::as_word()].
+#' Wrappers for [officer::body_add_par()] and [officer::body_add_xml()] that use
+#' the [cursor_docx()] helper functions to allow input of filename and path and
+#' use of multiple options for cursor placement. [add_text_to_body()] passes
+#' value to [glue::glue()] to add support for glue string interpolation.
+#' [add_gt_to_body()] converts gt tables to OOXML with [gt::as_word()]. If `pos
+#' = NULL`, [add_to_body()] calls [officer::body_add()] instead of
+#' [officer::body_add_par()].
+#'
+#' @details Using [add_vec_to_body()]
+#'
+#' [add_vec_to_body()] supports value vectors of length 1 or longer. If value is
+#' named, the names are assumed to be keywords indicating the cursor position
+#' for adding each value in the vector. If value is not named, a keyword
+#' parameter with the same length as value must be provided.
+#'
 #'
 #' @inheritParams cursor_docx
 #' @inheritParams read_docx_ext
@@ -17,30 +27,36 @@
 #' @returns A rdocx object with added xml, gt tables, or paragraphs of text.
 #' @export
 #' @importFrom officer body_add body_add_xml
-#' @importFrom cli cli_alert_warning
+#' @importFrom cli cli_abort cli_alert_warning
 add_to_body <- function(docx,
                         keyword = NULL,
                         id = NULL,
                         index = NULL,
                         value = NULL,
                         str = NULL,
+                        pos = "after",
                         ...) {
   check_docx(docx)
-  docx <- cursor_docx(docx, keyword, id, index)
 
-  if (!is.null(value)) {
-    return(officer::body_add(docx, value, ...))
+  if ((!is.null(str) & !is.null(value)) | is_all_null(c(str, value))) {
+    cli::cli_abort(
+      "Either {.arg str} or {.arg value} must be provided."
+    )
+  }
+
+  if (!is_all_null(c(keyword, id, index))) {
+    docx <- cursor_docx(docx, keyword, id, index)
   }
 
   if (!is.null(str)) {
-    return(officer::body_add_xml(docx, str, ...))
+    return(officer::body_add_xml(docx, str, pos))
   }
 
-  cli::cli_alert_warning(
-    "{.arg value} or {.arg str} are required to modify the {.arg docx} object."
-  )
+  if (!is.null(pos) & !is.null(value)) {
+    return(officer::body_add_par(docx, value, pos = pos, ...))
+  }
 
-  docx
+  officer::body_add(docx, value, ...)
 }
 
 #' @inheritParams glue::glue
@@ -52,13 +68,67 @@ add_to_body <- function(docx,
 add_text_to_body <- function(docx,
                              value,
                              style = NULL,
+                             pos = "after",
                              .na = "NA",
                              .null = NULL,
                              .envir = parent.frame(),
                              ...) {
   rlang::check_required(value)
   value <- glue::glue(value, .na = .na, .null = .null, .envir = .envir)
-  add_to_body(docx, value = value, style = style, ...)
+  add_to_body(docx, value = value, style = style, pos = pos, ...)
+}
+
+#' @name add_vec_to_body
+#' @rdname add_to_body
+#' @param .f Any function that takes a docx, value, and keyword parameter and
+#'   returns a rdocx object. Defaults to [add_text_to_body()].
+#' @export
+#' @importFrom rlang check_required is_named
+add_vec_to_body <- function(docx,
+                            value,
+                            ...,
+                            .f = add_text_to_body) {
+  rlang::check_required(value)
+  .f <- rlang::as_function(.f)
+
+  if (!rlang::is_named(value)) {
+    value <- set_vec_value_names(value, ...)
+  }
+
+  for (i in seq_along(value)) {
+    docx <-
+      .f(
+        docx = docx,
+        value = value[[i]],
+        keyword = names(value)[[i]],
+        ...
+      )
+  }
+
+  docx
+}
+
+#' Set names for a value vector object
+#'
+#' @keywords internal
+#' @param nm Names for value.
+#' @param arg Name of the argument in ... to use as names for value when nm is `NULL`.
+#' @noRd
+#' @importFrom rlang list2 set_names
+#' @importFrom cli cli_abort
+set_vec_value_names <- function(value, nm = NULL, arg = "keyword", ...) {
+  if (is.null(nm)) {
+    params <- rlang::list2(...)
+    if (is.null(params[[arg]]) | (length(params[[arg]]) != length(value))) {
+      cli::cli_abort(
+        "{.arg value} must {.arg {arg}} must be be the same length as {.arg value}."
+      )
+    }
+    nm <- params[[arg]]
+  }
+
+
+  rlang::set_names(value, nm)
 }
 
 #' @inheritParams officer::body_add_par
@@ -66,14 +136,16 @@ add_text_to_body <- function(docx,
 #' @rdname add_to_body
 #' @export
 add_xml_to_body <- function(docx,
-                            str = NULL,
+                            str,
                             pos = "after",
                             ...) {
+  rlang::check_required(str)
   add_to_body(docx, str = str, pos = pos, ...)
 }
 
 #' @param gt_object A gt object converted to an OOXML string with
-#'   [gt::as_word()] then passed to [add_xml_to_body()]. Required.
+#'   [gt::as_word()] then passed to [add_xml_to_body()] as str parameter.
+#'   Required for [add_gt_to_body()].
 #' @name add_gt_to_body
 #' @rdname add_to_body
 #' @export
--- R/utils.R
+++ R/utils.R
@@ -1,2 +1,40 @@
 # @staticimports pkg:isstatic
 #   has_fileext is_all_null
+
+# ---
+# repo: r-lib/rlang
+# file: standalone-purrr.R
+# last-updated: 2023-02-23
+# license: https://unlicense.org
+# ---
+#' @keywords internal
+map_lgl <- function(.x, .f, ...) {
+  .rlang_purrr_map_mold(.x, .f, logical(1), ...)
+}
+
+#' @keywords internal
+#' @importFrom rlang as_function global_env
+.rlang_purrr_map_mold <- function(.x, .f, .mold, ...) {
+  .f <- as_function(.f, env = global_env())
+  out <- vapply(.x, .f, .mold, ..., USE.NAMES = FALSE)
+  names(out) <- names(.x)
+  out
+}
+
+#' @keywords internal
+discard <- function(.x, .p, ...) {
+  sel <- .rlang_purrr_probe(.x, .p, ...)
+  .x[is.na(sel) | !sel]
+}
+
+#' @importFrom rlang is_logical as_function
+#' @keywords internal
+.rlang_purrr_probe <- function(.x, .p, ...) {
+  if (is_logical(.p)) {
+    stopifnot(length(.p) == length(.x))
+    .p
+  } else {
+    .p <- as_function(.p, env = global_env())
+    map_lgl(.x, .p, ...)
+  }
+}
