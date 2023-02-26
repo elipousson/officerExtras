6504578234d452c8d94de73425fc069748b6c2fa
Eli Pousson <eli.pousson@gmail.com>
2023-02-25 22:04:50 -0500

    test: add write_docx test

---
+++ tests/testthat/test-write_docx.R
@@ -0,0 +1,17 @@
+test_that("write_docx works", {
+  docx <- read_docx_ext(
+    filename = "example.docx",
+    path = system.file("doc_examples", package = "officer"),
+    quiet = TRUE
+  )
+
+  withr::with_tempdir({
+    write_docx(docx, "example.docx")
+    expect_identical(
+      docx[["doc_obj"]],
+      read_docx_ext(
+        filename = "example.docx", quiet = TRUE
+      )[["doc_obj"]]
+    )
+  })
+})
