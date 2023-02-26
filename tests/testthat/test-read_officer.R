27ca17fc7bd07119f4efef7afe11d85adef09f79
Eli Pousson <eli.pousson@gmail.com>
2023-02-26 17:28:44 -0500

    refactor: rename read_docx.R to read_officer.R
    - refactor: rename read_docx.R to read_officer.R
    - test: expand tests for read_officer + rename test file to test-read_officer.R

--- tests/testthat/test-read_officer.R
+++ tests/testthat/test-read_officer.R
@@ -1,4 +1,4 @@
-test_that("read_docx_ext works", {
+test_that("read_officer works", {
   docx <- read_docx_ext(
     filename = "example.docx",
     path = system.file("doc_examples", package = "officer")
@@ -19,5 +19,23 @@ test_that("read_docx_ext works", {
 
   expect_message(
     read_docx_ext(filename = "test.docx", docx = docx, quiet = FALSE)
+  )
+
+  pptx <- read_pptx_ext(
+    filename = "example.pptx",
+    path = system.file("doc_examples", package = "officer")
+  )
+
+  expect_true(
+    inherits(pptx, "rpptx")
+  )
+
+  xlsx <- read_xlsx_ext(
+    filename = "template.xlsx",
+    path = system.file("template", package = "officer")
+  )
+
+  expect_true(
+    inherits(xlsx, "rxlsx")
   )
 })
