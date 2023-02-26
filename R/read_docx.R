2416b4f4c0e1eea9004a8ecb18d588e48e16636e
Eli Pousson <eli.pousson@gmail.com>
2023-02-26 17:22:43 -0500

    feat: convert read_docx_ext to general function
    - feat: convert read_docx_ext to more general read_officer functions
    - feat: add comparable read_pptx_ext and read_xlsx_ext functions

--- R/read_docx.R
+++ R/read_docx.R
@@ -1,40 +1,107 @@
-#' Read a docx file or use a rdox object if provided
+#' Read a docx, pptx, potx, or xlsx file or use an existing object from officer
+#' if provided
+#'
+#' [read_docx_ext()], [read_pptx_ext()], and [read_xlsx_ext()] are wrappers for
+#' [read_officer()] which is a wrapper for [officer::read_docx()],
+#' [officer::read_pptx()], and [officer::read_xlsx()]. This function uses both a
+#' filename and path (the original officer functions only use a path) and
+#' default to use a rdocx, rpptx, or rxlsx class object if provided.
 #'
 #' @param filename,path File name and path. Default: `NULL`
-#' @param docx rdocx class object, If docx is provided, filename and path are
-#'   ignored. Default: `NULL`
+#' @param x A rdocx, rpptx, or rxlsx class object, If docx is provided, filename
+#'   and path are ignored. Default: `NULL`
+#' @param docx,pptx,xlsx A rdocx, rpptx, or rxlsx class object passed to the x
+#'   parameter by the corresponding function. Defaults to `NULL`.
 #' @param quiet If `FALSE`, warn if docx is provided when filename and/or path
 #'   are also provided. Default: `TRUE`.
-#' @return A rdocx object.
+#' @return A rdocx, rpptx, or rxlsx object.
 #' @seealso
 #'  [officer::read_docx()]
-#' @rdname read_docx_ext
+#' @rdname read_officer
 #' @export
 #' @importFrom cli cli_alert_warning cli_alert_success symbol
 #' @importFrom officer read_docx
-read_docx_ext <- function(filename = NULL, path = NULL, docx = NULL, quiet = TRUE) {
-  if (is.null(docx)) {
-    docx <- officer::read_docx(
-      path = set_office_path(filename, path, fileext = "docx")
+read_officer <- function(filename = NULL,
+                         path = NULL,
+                         fileext = c("docx", "pptx", "xlsx"),
+                         x = NULL,
+                         arg = caller_arg(x),
+                         quiet = TRUE) {
+  if (is.null(x)) {
+    path <- set_office_path(filename, path, fileext = fileext)
+
+    x <- switch(fileext,
+      "docx" = officer::read_docx(path),
+      "pptx" = officer::read_pptx(path),
+      "xlsx" = officer::read_xlsx(path)
     )
   } else {
     if (!is_all_null(c(filename, path)) & isFALSE(quiet)) {
       cli::cli_alert_warning(
-        "{.arg filename} and {.arg path} are ignored if {.arg docx} is provided."
+        "{.arg filename} and {.arg path} are ignored if {.arg {arg}} is provided."
       )
     }
 
-    check_docx(docx)
+    check_officer(x, what = paste0("r", fileext))
   }
 
   if (isFALSE(quiet)) {
     if (!is.null(filename)) {
       cli::cli_alert_success("Reading {.filename {filename}}{cli::symbol$ellipsis}")
     }
-    list_doc_properties(docx, filename)
+
+    list_doc_properties(x, filename)
   }
 
-  invisible(docx)
+  invisible(x)
+}
+
+#' @name read_docx_ext
+#' @rdname read_officer
+#' @export
+read_docx_ext <- function(filename = NULL,
+                          path = NULL,
+                          docx = NULL,
+                          quiet = TRUE) {
+  read_officer(
+    filename = filename,
+    path = path,
+    fileext = "docx",
+    x = docx,
+    quiet = quiet
+  )
+}
+
+#' @name read_pptx_ext
+#' @rdname read_officer
+#' @export
+read_pptx_ext <- function(filename = NULL,
+                          path = NULL,
+                          pptx = NULL,
+                          quiet = TRUE) {
+  read_officer(
+    filename = filename,
+    path = path,
+    fileext = "pptx",
+    x = pptx,
+    quiet = quiet
+  )
+}
+
+#' @name read_xlsx_ext
+#' @rdname read_officer
+#' @export
+read_xlsx_ext <- function(filename = NULL,
+                          path = NULL,
+                          xlsx = NULL,
+                          quiet = TRUE) {
+  read_officer(
+    filename = filename,
+    path = path,
+    fileext = "xlsx",
+    x = xlsx,
+    quiet = quiet
+  )
 }
 
 #' List document properties for a rdocx or rpptx object
@@ -44,8 +111,8 @@ read_docx_ext <- function(filename = NULL, path = NULL, docx = NULL, quiet = TRU
 #' @importFrom officer doc_properties
 #' @importFrom cli cli_rule symbol cli_dl
 #' @importFrom rlang set_names
-list_doc_properties <- function(docx, filename = NULL) {
-  props <- officer::doc_properties(docx)
+list_doc_properties <- function(x, filename = NULL) {
+  props <- officer::doc_properties(x)
   if (!is.null(filename)) {
     cli::cli_rule("{cli::symbol$info} {.filename {filename}} properties:")
   } else {
@@ -71,6 +138,12 @@ set_office_path <- function(filename = NULL,
     path <- filename
   } else {
     path <- file.path(path, filename)
+  }
+
+  fileext <- match.arg(fileext)
+
+  if ((fileext == "pptx") & is_fileext_path(path, "potx")) {
+    fileext <- "potx"
   }
 
   check_office_fileext(
