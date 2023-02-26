8ec86c82807d0637c9eb10b48dd475b0be148a2c
Eli Pousson <eli.pousson@gmail.com>
2023-02-25 21:37:15 -0500

    docs: update write_docx docs
    refactor: write_docx invisibly returns docx

--- R/write_docx.R
+++ R/write_docx.R
@@ -2,9 +2,11 @@
 #'
 #' @param docx A rdocx object to save.
 #' @param path File path.
-#' @param overwrite If `FALSE` and file exists, abort save function. If `TRUE`,
-#'   save file even if file exists at path.
+#' @param overwrite If `TRUE` (default), remove file at path if it already
+#'   exists. If `FALSE` and file exists, this function aborts.
 #' @export
+#' @returns Returns rdocx input object (invisibly) and writes the rdocx object
+#'   to a file with a name and location matching path.
 #' @importFrom rlang check_required check_installed
 #' @importFrom cli cli_abort
 write_docx <- function(docx, path, overwrite = TRUE) {
@@ -22,4 +24,6 @@ write_docx <- function(docx, path, overwrite = TRUE) {
   }
 
   print(docx, target = path)
+
+  invisible(docx)
 }
