51234151799dbf267476e90e8daf04ccccb4803a
Eli Pousson <eli.pousson@gmail.com>
2023-02-26 21:43:12 -0500

    chore: correct location of export roxygen2 tag

--- R/write_officer.R
+++ R/write_officer.R
@@ -6,9 +6,9 @@
 #' @param overwrite If `TRUE` (default), remove file at path if it already
 #'   exists. If `FALSE` and file exists, this function aborts.
 #' @inheritDotParams officer::set_doc_properties -x
-#' @export
 #' @returns Returns rdocx input object (invisibly) and writes the rdocx object
 #'   to a file with a name and location matching path.
+#' @export
 #' @importFrom rlang check_required check_installed current_call
 #' @importFrom cli cli_abort
 #' @importFrom officer set_doc_properties
