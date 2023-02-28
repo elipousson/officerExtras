93b0a069933525891ac20348f1d60092d86103c9
Eli Pousson <eli.pousson@gmail.com>
2023-02-28 09:44:24 -0500

    docs: update write_officer docs

--- R/write_officer.R
+++ R/write_officer.R
@@ -1,4 +1,4 @@
-#' Write a rdocx object to a docx file
+#' Write a rdocx, rpptx, or rxlsx object to a file
 #'
 #' @param x A rdocx, rpptx, or rxlsx object to save. Document properties won't
 #'   be set for rxlsx objects.
@@ -6,13 +6,16 @@
 #' @param overwrite If `TRUE` (default), remove file at path if it already
 #'   exists. If `FALSE` and file exists, this function aborts.
 #' @inheritDotParams officer::set_doc_properties -x
-#' @returns Returns rdocx input object (invisibly) and writes the rdocx object
-#'   to a file with a name and location matching path.
+#' @returns Returns the input object (invisibly) and writes the rdocx, rpptx, or
+#'   rxlsx object to a file with a name and location matching the provided path.
 #' @export
 #' @importFrom rlang check_required check_installed current_call
 #' @importFrom cli cli_abort
 #' @importFrom officer set_doc_properties
-write_officer <- function(x, path, overwrite = TRUE, ...) {
+write_officer <- function(x,
+                          path,
+                          overwrite = TRUE,
+                          ...) {
   rlang::check_required(x)
   rlang::check_required(path)
 
@@ -25,9 +28,8 @@ write_officer <- function(x, path, overwrite = TRUE, ...) {
 
   is_xlsx <- isTRUE(is_fileext_path(path, "xlsx"))
 
-  params <- rlang::list2(...)
+  if (isFALSE(rlang::is_empty(rlang::list2(...))) & !is_xlsx) {
 
-  if (isFALSE(rlang::is_empty(params)) & !is_xlsx) {
     x <-
       officer::set_doc_properties(
         x,
