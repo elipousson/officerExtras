6dea66f48729510d7360b1c1ef0123b9f585b56e
Eli Pousson <eli.pousson@gmail.com>
2023-03-04 21:08:38 -0500

    test: add officer_tables test

---
+++ tests/testthat/test-officer_tables.R
@@ -0,0 +1,65 @@
+test_that("officer_tables works", {
+  pptx <-
+    read_pptx_ext(
+      system.file("doc_examples/example.pptx", package = "officer")
+    )
+
+  expect_type(
+    officer_tables(pptx),
+    "list"
+  )
+
+  pptx_tab <- officer_tables(pptx)[[1]]
+
+  expect_s3_class(
+    pptx_tab,
+    "data.frame"
+  )
+
+  expect_named(
+    pptx_tab,
+    c("Header 1 ", "Header 2", "Header 3")
+  )
+
+  expect_named(
+    officer_tables(pptx, has_header = FALSE)[[1]],
+    c("1", "2", "3")
+  )
+
+  expect_error(
+    officer_tables("pptx"),
+    "must be a"
+  )
+
+  withr::with_tempdir({
+    skip_if_not_installed("gt")
+
+    docx <- read_docx_ext(
+      filename = "example.docx",
+      path = system.file("doc_examples", package = "officer")
+    )
+
+    tab_1 <-
+      gt::gt(
+        gt::exibble,
+        rowname_col = "row",
+        groupname_col = "group"
+      )
+
+    write_officer(
+      add_gt_to_body(
+        docx,
+        tab_1,
+        keyword = "Sub title 1"
+      ),
+      "test-officer_tables.docx"
+    )
+
+    docx <- read_officer("test-officer_tables.docx")
+
+    expect_s3_class(
+      officer_tables(docx)[[1]],
+      "data.frame"
+    )
+  })
+})
