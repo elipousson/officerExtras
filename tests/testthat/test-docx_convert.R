e64e5e62a1988b7e8b5f8fa2edf5d76c6274b13e
Eli Pousson <eli.pousson@gmail.com>
2023-03-18 16:18:22 -0400

    test: skip docx_convert test on GH action

--- tests/testthat/test-docx_convert.R
+++ tests/testthat/test-docx_convert.R
@@ -1,29 +1,30 @@
 test_that("docx_convert works", {
-    docx <- read_officer(
-      system.file("doc_examples/example.docx", package = "officer")
+  skip_on_ci()
+  docx <- read_officer(
+    system.file("doc_examples/example.docx", package = "officer")
+  )
+
+  withr::with_tempdir({
+    path <- getwd()
+
+    docx_convert(
+      docx,
+      output = "test-docx.html",
+      path = path
     )
 
-    withr::with_tempdir({
-      path <- getwd()
+    expect_true(
+      file.exists(file.path(path, "test-docx.html"))
+    )
 
-      docx_convert(
-        docx,
-        output = "test-docx.html",
-        path = path
-      )
+    docx_convert(
+      system.file("doc_examples/example.docx", package = "officer"),
+      output = "test-docx.pdf",
+      path = getwd()
+    )
 
-      expect_true(
-        file.exists(file.path(path, "test-docx.html"))
-      )
-
-      docx_convert(
-        system.file("doc_examples/example.docx", package = "officer"),
-        output = "test-docx.pdf",
-        path = getwd()
-      )
-
-      expect_true(
-        file.exists(file.path(path, "test-docx.pdf"))
-      )
-    })
+    expect_true(
+      file.exists(file.path(path, "test-docx.pdf"))
+    )
+  })
 })
