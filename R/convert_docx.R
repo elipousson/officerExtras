bd429989d11c3c75ada6ea952b8f455aae7a2ab3
Eli Pousson <eli.pousson@gmail.com>
2023-03-20 12:22:20 -0400

    feat: add extract_media parameter

--- R/convert_docx.R
+++ R/convert_docx.R
@@ -1,15 +1,22 @@
 #' Convert an rodcx object or Word file to another format with pandoc
 #'
 #' Use [rmarkdown::pandoc_convert()] to convert a rodcx object or Word file to
-#' another format with pandoc.
+#' another format with pandoc. If the "to" parameter contains a file extension,
+#' it is assumed to be an output file name.
 #'
 #' @param docx A rdocx object or path to a docx file. Optional if input is
 #'   provided.
 #' @inheritParams rmarkdown::pandoc_convert
 #' @inheritParams write_officer
-#' @inheritDotParams rmarkdown::pandoc_convert -verbose
+#' @param path File path for converted file passed to wd parameter of
+#'   [rmarkdown::pandoc_convert()]. If docx is a rdocx object, path defaults to
+#'   current working directory instead of the base directory of the input file
+#'   path.
+#' @inheritDotParams rmarkdown::pandoc_convert -verbose -wd
 #' @param quiet If `TRUE`, suppress alert messages and pass `FALSE` to verbose
 #'   parameter of [rmarkdown::pandoc_convert()]. Defaults `TRUE`.
+#' @param extract_media If `TRUE`, append `"--extract-media=."` to
+#'   options. Defaults to `FALSE`.
 #' @inherit rmarkdown::pandoc_convert details
 #' @returns Executes a call to pandoc using [rmarkdown::pandoc_convert()] to
 #'   create a file from an officer object or a docx file.
@@ -20,10 +27,15 @@
 #'     system.file("doc_examples/example.docx", package = "officer")
 #'   )
 #'
+#'   convert_docx(
+#'       docx,
+#'       to = "markdown"
+#'     )
+#'
 #'   withr::with_tempdir({
 #'     convert_docx(
 #'       docx,
-#'       output = "docx.md"
+#'       output = "docx.html"
 #'     )
 #'   })
 #' }
@@ -38,8 +50,10 @@ convert_docx <- function(docx,
                          input = NULL,
                          output = NULL,
                          path = NULL,
-                         quiet = TRUE,
+                         options = NULL,
+                         extract_media = TRUE,
                          overwrite = TRUE,
+                         quiet = TRUE,
                          ...) {
   rlang::check_installed("rmarkdown")
 
@@ -55,23 +69,50 @@ convert_docx <- function(docx,
 
     write_officer(
       docx,
-      path = input,
-      overwrite = overwrite
+      path = input
     )
+
+    path <- path %||% getwd()
   } else if (is.null(input)) {
     rlang::check_required(docx)
     check_docx_fileext(docx)
     input <- docx
   }
 
+  if (is_bool(extract_media) && is_true(extract_media)) {
+    options <- c(options, "--extract-media=.")
+  }
+
   if (!is.null(path) && !dir.exists(path)) {
     dir.create(path)
+  }
+
+  if (has_fileext(to) && is.null(output)) {
+    output <- to
+    to <- NULL
+  }
+
+  if (!is.null(output)) {
+    output_exists <- FALSE
+    if (is.null(path)) {
+      output_exists <- file.exists(output)
+    } else {
+      output_exists <- file.exists(file.path(path, output))
+    }
+
+    if (is_true(output_exists) && is_false(overwrite)) {
+      cli::cli_abort(
+        c("{.arg output} {.filename {output}} already exists.",
+          "*" = "Set {.code overwrite = TRUE} to replace existing file.")
+      )
+    }
   }
 
   rmarkdown::pandoc_convert(
     input = input,
     to = to,
     output = output,
+    options = options,
     verbose = is_false(quiet),
     wd = path,
     ...
