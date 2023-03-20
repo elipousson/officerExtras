9cd576db1a7fc51e581726d56fc100a1bb561f79
Eli Pousson <eli.pousson@gmail.com>
2023-03-20 12:34:39 -0400

    refactor: update imports

--- R/officerExtras-package.R
+++ R/officerExtras-package.R
@@ -4,6 +4,13 @@
 ## usethis namespace: start
 #' @importFrom rlang .data
 #' @importFrom rlang %||%
+#' @importFrom rlang is_true
+#' @importFrom rlang is_false
+#' @importFrom rlang is_bool
+#' @importFrom rlang check_required
+#' @importFrom rlang is_installed
+#' @importFrom rlang check_installed
+#' @importFrom rlang has_name
 #' @importFrom cli cli_abort
 ## usethis namespace: end
 NULL
