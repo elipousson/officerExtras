8714202295d3ddcec9c0c9056890febff29bfe2b
Eli Pousson <eli.pousson@gmail.com>
2023-06-07 21:37:19 -0400

    feat: add type_convert + nm parameters to officer_tables

--- R/officer_tables.R
+++ R/officer_tables.R
@@ -1,19 +1,22 @@
 #' Get tables from a rdocx or rpptx object
 #'
-#' Get one or more tables from a rdocx or rpptx object. Functions based on
-#' example code on extracting Word document and PowerPoint slides in the
-#' [officeverse
+#' Get one or more tables from a rdocx or rpptx object. These functions are
+#' based on example code on extracting Word document and PowerPoint slides in
+#' the [officeverse
 #' documentation](https://ardata-fr.github.io/officeverse/extract-content.html#word-tables).
+#' #' [officer_table()] is a lower-level helper function to extract a single
+#' table from a document. Some additional features including the type_convert
+#' parameter and the addition of doc_index values as the default names for the
+#' returned list of tables are based on [this blog post by Matt
+#' Dray](https://www.rostrum.blog/2023/06/07/rectangular-officer/).
 #'
-#' [officer_table()] is a lower-level helper function to extract a single table
-#' from a document.
-#'
-#' @param x A rdocx or rpptx object or a data.frame created with
-#'   [officer_summary()]
+#' @param x A rdocx or rpptx object or a data frame created with
+#'   [officer_summary()].
 #' @param index A index value matching a doc_index value for a table in the
-#'   summary data.frame, Default: `NULL`
-#' @param has_header If `TRUE`, tables are expected to have implicit headers
-#'   even if the object Default: `TRUE`
+#'   summary data frame, Default: `NULL`
+#' @param has_header If `TRUE` (default), tables are expected to have implicit
+#'   headers even if the Word table does not have an explicit header row. If
+#'   `FALSE`, only explicit header rows will be used as column names.
 #' @param col If col is supplied, [officer_table()] passes col and the
 #'   additional parameters in ... to [fill_with_pattern()]. This allows the
 #'   addition of preceding headings or captions as a column within the
@@ -21,9 +24,14 @@
 #'   and may be modified or removed. Defaults to `NULL`.
 #' @param ... Additional parameters passed to [fill_with_pattern()].
 #' @param stack If `TRUE` and all tables share the same number of columns,
-#'   return a combined data.frame instead of a list object.
+#'   return a single combined data frame instead of a list. Defaults to `FALSE`.
+#' @param type_convert If `TRUE`, convert columns for the returned data frames
+#'   to the appropriate type using [utils::type.convert()].
+#' @param nm Names to use for returned list of tables. If `NULL` (default), the
+#'   names are set to the doc_index values using the pattern
+#'   "doc_index_<doc_index_number>".
 #' @inheritParams rlang::args_error_context
-#' @return A data.frame or list of data.frame (or list objects).
+#' @return A list of data frames or, if stack is `TRUE`, a single data frame.
 #' @examples
 #' docx <- read_docx_ext(
 #'   filename = "example.docx",
@@ -40,31 +48,40 @@
 #' officer_tables(pptx)[[1]]
 #'
 #' @rdname officer_tables
+#' @seealso [docxtractr::docx_extract_all()]
 #' @export
-#' @importFrom rlang has_name %||%
+#' @importFrom utils type.convert
+#' @importFrom rlang set_names
 officer_tables <- function(x,
                            index = NULL,
                            has_header = TRUE,
                            col = NULL,
+                           preserve = FALSE,
                            ...,
                            stack = FALSE,
+                           type_convert = FALSE,
+                           nm = NULL,
                            call = caller_env()) {
   check_required(x)
   if (is_officer(x, c("rdocx", "rpptx"))) {
-    x <- officer_summary(x)
+    x <- officer_summary(x, preserve = preserve, call = call)
   }
 
   if (!has_name(x, "content_type") || !is.data.frame(x)) {
     cli_abort(
-      "{.arg x} must be a {cli_vec_cls(c('rdocx', 'rpptx'))} object or a
-      {.cls data.frame} created with {.fn officer_summary}",
+      "{.arg x} must be a {cli_vec_cls(c('rdocx', 'rpptx'))} object or a data
+      frame created with {.fn officer_summary}",
       call = call
     )
   }
 
+  index <- index %||% officer_table_index(x)
+
+  tables <- vector("list", length(index))
+
   tables <-
     map(
-      index %||% officer_table_index(x),
+      index,
       function(i) {
         officer_table(
           x = x,
@@ -76,8 +93,8 @@ officer_tables <- function(x,
       }
     )
 
-  if (has_length(tables, 1)) {
-    return(tables[[1]])
+  if (type_convert) {
+    tables <- lapply(tables, utils::type.convert, as.is = TRUE)
   }
 
   if (stack) {
@@ -93,14 +110,14 @@ officer_tables <- function(x,
     return(do.call("rbind", tables))
   }
 
-  tables
+  rlang::set_names(tables, nm %||% glue("doc_index_{index}"))
 }
 
 #' @rdname officer_tables
 #' @name officer_table
 #' @export
-#' @importFrom rlang set_names
-#' @importFrom utils tail head
+#' @importFrom rlang has_name set_names
+#' @importFrom utils head tail
 officer_table <- function(x,
                           index = NULL,
                           has_header = TRUE,
@@ -194,6 +211,7 @@ officer_table <- function(x,
 
 #' @keywords internal
 #' @noRd
+#' @importFrom rlang has_name
 officer_table_index <- function(x) {
   tables <- subset_type(x, "table cell")
   if (rlang::has_name(x, "doc_index")) {
@@ -208,7 +226,7 @@ officer_table_index <- function(x) {
 officer_table_pivot <- function(x,
                                 call = caller_env()) {
   tbl <-
-    as.data.frame(
+    data.frame(
       tapply(
         x[["text"]],
         list(
@@ -218,6 +236,8 @@ officer_table_pivot <- function(x,
         FUN = I
       )
     )
+
+  rownames(tbl) <- NULL
 
   tbl
 }
--- tests/testthat/test-officer_tables.R
+++ tests/testthat/test-officer_tables.R
@@ -23,7 +23,7 @@ test_that("officer_tables works", {
 
   expect_named(
     officer_tables(pptx, has_header = FALSE)[[1]],
-    c("1", "2", "3")
+    c("X1", "X2", "X3")
   )
 
   expect_error(
@@ -57,9 +57,15 @@ test_that("officer_tables works", {
 
     docx <- read_officer("test-officer_tables.docx")
 
+    type_convert_tbl <- officer_tables(docx, type_convert = TRUE)[[1]]
+
     expect_s3_class(
-      officer_tables(docx)[[1]],
+      type_convert_tbl,
       "data.frame"
+    )
+    expect_type(
+      c(type_convert_tbl[[2]], type_convert_tbl[[8]]),
+      "double"
     )
   })
 })
