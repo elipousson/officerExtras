48ea9ba1d91c5ce2ac4dfa2f7495ac7871192754
Eli Pousson <eli.pousson@gmail.com>
2023-07-24 20:25:59 -0400

    refactor: add lifecycle to Imports

--- R/officerExtras-package.R
+++ R/officerExtras-package.R
@@ -3,18 +3,19 @@
 
 ## usethis namespace: start
 #' @import rlang
+#' @importFrom cli cli_abort
+#' @importFrom lifecycle deprecated signal_stage
 #' @importFrom rlang .data
 #' @importFrom rlang %||%
-#' @importFrom rlang is_true
-#' @importFrom rlang is_false
-#' @importFrom rlang is_bool
 #' @importFrom rlang caller_arg
 #' @importFrom rlang caller_env
-#' @importFrom rlang check_required
-#' @importFrom rlang is_installed
 #' @importFrom rlang check_installed
-#' @importFrom rlang has_name
+#' @importFrom rlang check_required
 #' @importFrom rlang env_get_list
-#' @importFrom cli cli_abort
+#' @importFrom rlang has_name
+#' @importFrom rlang is_bool
+#' @importFrom rlang is_false
+#' @importFrom rlang is_installed
+#' @importFrom rlang is_true
 ## usethis namespace: end
 NULL
