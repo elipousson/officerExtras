fc02b3a56c0b8cd13c28b03c5f341fa93b8ad068
your name <eli.pousson@gmail.com>
2023-11-22 21:25:32 -0500

    test: add test for reduce_officer

---
+++ tests/testthat/test-reduce_officer.R
@@ -0,0 +1,22 @@
+test_that("reduce_officer works", {
+
+  example_path <- system.file("doc_examples/example.docx", package = "officer")
+  x <- reduce_officer(example_path, value = LETTERS)
+
+  expect_s3_class(
+    x,
+    "rdocx"
+  )
+
+  withr::with_tempdir({
+    x <- reduce_officer(
+      example_path,
+      value = LETTERS,
+      .path = "example_file_with_letters.docx"
+    )
+
+    expect_true(
+      file.exists("example_file_with_letters.docx")
+    )
+  })
+})
