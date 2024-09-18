4cd4e9ee36f28cdb38f40e89a358832f3fc351f4
Eli Pousson <eli.pousson@gmail.com>
2024-09-17 20:33:29 -0400

    Add test for use_doc_version

---
+++ tests/testthat/test-use_doc_version.R
@@ -0,0 +1,29 @@
+test_that("use_doc_version works", {
+
+  example_path <- system.file("doc_examples/example.docx", package = "officer")
+
+  withr::with_tempdir({
+    use_doc_version(
+      filename = example_path,
+      which = "major",
+      path = getwd()
+    )
+
+    expect_true(
+      file.exists(
+        "example_1.0.0.docx"
+      )
+    )
+
+    use_doc_version(
+      filename = "example_1.0.0.docx",
+      which = "minor"
+    )
+
+    expect_true(
+      file.exists(
+        "example_1.1.0.docx"
+      )
+    )
+  })
+})
