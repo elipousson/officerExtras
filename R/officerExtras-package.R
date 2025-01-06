a1ce7ae40d14aaf1aaf2cc8175f470154e1df20c
Eli Pousson <eli.pousson@gmail.com>
2025-01-05 21:17:35 -0500

    Drop imports for rlang functions

--- R/officerExtras-package.R
+++ R/officerExtras-package.R
@@ -3,19 +3,7 @@
 
 ## usethis namespace: start
 #' @import rlang
-#' @importFrom cli cli_abort
+#' @importFrom cli cli_abort cli_bullets
 #' @importFrom lifecycle deprecated signal_stage
-#' @importFrom rlang .data
-#' @importFrom rlang %||%
-#' @importFrom rlang caller_arg
-#' @importFrom rlang caller_env
-#' @importFrom rlang check_installed
-#' @importFrom rlang check_required
-#' @importFrom rlang env_get_list
-#' @importFrom rlang has_name
-#' @importFrom rlang is_bool
-#' @importFrom rlang is_false
-#' @importFrom rlang is_installed
-#' @importFrom rlang is_true
 ## usethis namespace: end
 NULL
